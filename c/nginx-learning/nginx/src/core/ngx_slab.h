d628795d800fdb149350bb566014c466c525e730
clr7™ <46675562+crl7-key@users.noreply.github.com>
2020-10-08 14:09:36 +0800

    🍊 ngx_slab.h learning

--- c/nginx-learning/nginx/src/core/ngx_slab.h
+++ c/nginx-learning/nginx/src/core/ngx_slab.h
@@ -15,6 +15,7 @@
 
 typedef struct ngx_slab_page_s  ngx_slab_page_t;
 
+// slab内存页链表结构体
 struct ngx_slab_page_s {
     uintptr_t         slab;
     ngx_slab_page_t  *next;
@@ -37,11 +38,11 @@ typedef struct {
     size_t            min_size;
     size_t            min_shift;
 
-    ngx_slab_page_t  *pages;
+    ngx_slab_page_t  *pages;  // 指向page数组的第一个成员page[0]的地址
     ngx_slab_page_t  *last;
-    ngx_slab_page_t   free;
+    ngx_slab_page_t   free;  // 注意它是一个变量,free.next指向一个空闲的page[?]
 
-    ngx_slab_stat_t  *stats;
+    ngx_slab_stat_t  *stats; // 真正内存页的开始地址,每个内存页默认4K字节  
     ngx_uint_t        pfree;
 
     u_char           *start;
@@ -59,13 +60,29 @@ typedef struct {
 } ngx_slab_pool_t;
 
 
+// slab内存页面大小初始化
 void ngx_slab_sizes_init(void);
+
+// 初始化slab结构,按slot和page管理这块共享内存,best-fit,之前需要初始化min_shift和end
+// 自己使用可以把min_shift适当调整改大一点,分析以64位系统,4m共享内存为例,缺一个reinit函数,简单地清空共享内存
 void ngx_slab_init(ngx_slab_pool_t *pool);
+
+// 加锁分配内存
 void *ngx_slab_alloc(ngx_slab_pool_t *pool, size_t size);
+
+// 不加锁分配内存,超过2k则直接分配整页
 void *ngx_slab_alloc_locked(ngx_slab_pool_t *pool, size_t size);
+
+// 加锁分配内存并清空
 void *ngx_slab_calloc(ngx_slab_pool_t *pool, size_t size);
+
+// 不加锁分配内存并清空
 void *ngx_slab_calloc_locked(ngx_slab_pool_t *pool, size_t size);
+
+// 加锁释放内存
 void ngx_slab_free(ngx_slab_pool_t *pool, void *p);
+
+// 不加锁分配内存
 void ngx_slab_free_locked(ngx_slab_pool_t *pool, void *p);
 
 
