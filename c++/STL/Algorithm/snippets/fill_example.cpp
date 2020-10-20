2733e38fb54defe5e644169970d6ae8e8507d3e7
o7key <317998277@qq.com>
2020-10-20 21:03:43 +0800

    🍊 fill 将 [first, last) 内的所有元素改填新值。

---
+++ c++/STL/Algorithm/snippets/fill_example.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int main()
+{
+    std::vector<int> v{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+ 
+    std::fill(v.begin(), v.end(), -1);
+ 
+    for (auto elem : v) {
+        std::cout << elem << " ";
+    }
+    std::cout << "\n";
+
+    //output : -1 -1 -1 -1 -1 -1 -1 -1 -1 -1
+    return 0;
+}
