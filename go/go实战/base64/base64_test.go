7aa08380519503f6c1587a6757b9157e05b173da
o7key <317998277@qq.com>
2021-01-07 11:53:04 +0800

    🍊 golang 中使用base64编解码

---
+++ go/go实战/base64/base64_test.go
@@ -0,0 +1,36 @@
+package base64_test
+
+import (
+	"encoding/base64"
+	"fmt"
+	"testing"
+)
+
+func TestBase64(t *testing.T) {
+
+	s := "heal the world, make it a better place"
+
+	encodeStd := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+	s64 := base64.NewEncoding(encodeStd).EncodeToString([]byte(s))
+
+	fmt.Println("base64.NewEncoding(encodeStd).EncodeToString")
+	fmt.Println(len(s))
+	fmt.Println(len(s64))
+	fmt.Println(s)
+	fmt.Println(s64)
+
+	// encode
+	s64_std := base64.StdEncoding.EncodeToString([]byte(s))
+	fmt.Println("base64.StdEncoding.EncodeToString")
+	fmt.Println(len(s))
+	fmt.Println(len(s64_std))
+	fmt.Println(s)
+	fmt.Println(s64_std)
+
+	// decode
+	s64_decode, err := base64.StdEncoding.DecodeString(s64_std)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(s64_decode))
+}
