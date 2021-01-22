cb11c5f2ffe4170788538ee249c4c0d7e5d52b4c
o7key <317998277@qq.com>
2021-01-22 17:52:27 +0800

    🍊 显示系统变量和转义字符

---
+++ linux/shell/basic/show_sys_var.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#display user information from system
+
+echo "User info username is: $USER"
+
+echo UID:$UID
+
+echo HOME: $HOME
+
+# 换行
+echo -e '\n'      
+
+echo 'The cost of the item is $15'
+
