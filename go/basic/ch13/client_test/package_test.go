fdee6b5d8fd540f3dd788cb36ac917088ba58038
o7key <317998277@qq.com>
2021-02-02 19:04:32 +0800

    🍊 package

--- go/basic/ch13/client_test/package_test.go
+++ go/basic/ch13/client_test/package_test.go
@@ -3,10 +3,11 @@ package client_test
 import (
 	"fmt"
 	series "test/series_test"
+
 	"testing"
 )
 
 func TestPackage(t *testing.T) {
 	series.GetFibonacciSerie(5)
-	fmt.Println(series.square(5))
+	fmt.Println(series.Square(5))
 }
